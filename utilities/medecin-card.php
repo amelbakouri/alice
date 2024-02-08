471ab985691b7bb3961f3bf23a0e40668be84235
amelbakouri <amelbakouri4@gmail.com>
2024-02-08 14:16:04 +0100

    changement de h5 à h4 pour les titres de card

--- utilities/medecin-card.php
+++ utilities/medecin-card.php
@@ -7,7 +7,7 @@ foreach ($docteurs as $docteur) :
     <div class="card col-6 text-center m-3 p-0 rounded-0 border-0 text-blue" style="width: 300px;">
         <img src="/img/docteurs/<?= $docteur["pathImg"] ?>" class="card-img-top rounded-0" alt="<?= $docteur["name"] ?>">
         <div class="card-body">
-            <h5 class="card-title"><?= $docteur["name"] ?></h5>
+            <h4 class="card-title"><?= $docteur["name"] ?></h5>
             <p class="card-text"><?= $docteur["description"] ?></p>
         </div>
     </div>
--- utilities/produits-card.php
+++ utilities/produits-card.php
@@ -6,7 +6,7 @@
 <div class="card col text-center m-3 p-0 rounded-0 border-0 text-blue" style="width: 300px;">
       <img src="./img/produits/<?= $medicament["pathImg"] ?>" class="card-img-top rounded-0" alt="<?= $medicament["name"] ?>">
       <div class="card-body">
-        <h5 class="card-title"><?= $medicament["name"] ?></h5>
+        <h4 class="card-title"><?= $medicament["name"] ?></h5>
         <p class="card-text"><?= $medicament["description"] ?></p>
         <p class="card-text"><small class="text-muted">Prix: <?= $medicament["prix"] ?></small></p>
         <a href="produit.php?id=<?= $medicament['id'] ?>" class="btn bg-blue text-white rounded-0">Acheter</a>
